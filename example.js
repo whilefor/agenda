21e3c19c10cb9490e7931ab8990221d809d80fb9
whilefor <469676693@qq.com>
2015-10-16 05:42:23 +0800

    startTime and endTime update

--- example.js
+++ example.js
@@ -5,14 +5,14 @@ var agenda = new Agenda({
     }
 });
 //ssh
-var Client = require('ssh2').Client;
+//var Client = require('ssh2').Client;
 var MongoClient = require('mongodb').MongoClient;
 
 
 var job1 = {
     jobName: 'job111',
     jobURL:'http://www.baidu.com',
-    jobStartTime: '',
+    jobStartTime: '2015-10-16 04:59',
     jobEndTime: '2016-10-15 23:26',
     jobTimeInterval: '10',
     jobServers: null
@@ -35,8 +35,16 @@ MongoClient.connect('mongodb://localhost:27017/agendaTest', function(err, db) {
         runJob(job1, jobsLogCollection);
         done();
     });
+    // var job = agenda.create(job1.jobName, job1);
+    // job.repeatEvery(job1.jobTimeInterval + ' seconds');
+    // job.save(); 
+    // agenda.start();
+    
+
+
     agenda.every(job1.jobTimeInterval + ' seconds', job1.jobName, job1, job1.jobStartTime, job1.jobEndTime);
     agenda.start();
+    
 
     // agenda.define(job2.jobName, function(_job, done) {
     //     console.log(job2.jobName, ' job running: ', new Date());
