37f1689f7708d9527ec9bcd73978d6f9e2394d0b
wanglin <wanglin02@tudou.com>
2015-10-15 19:31:42 +0800

    add startTime and endTime for 'agenda.every' function

---
+++ example.js
@@ -0,0 +1,55 @@
+var Agenda = require('./index.js');
+var agenda = new Agenda({
+    db: {
+        address: 'localhost:27017/agendaTest'
+    }
+});
+//ssh
+var Client = require('ssh2').Client;
+var MongoClient = require('mongodb').MongoClient;
+
+
+var job1 = {
+    jobName: 'job111',
+    jobURL:'http://www.baidu.com',
+    jobStartTime: '2015-10-15 19:21',
+    jobEndTime: '2016-10-15 19:26',
+    jobTimeInterval: '10',
+    jobServers: null
+}
+
+var job2 = {
+    jobName: 'job222',
+    jobURL:'http://www.baidu.com',
+    jobStartTime: '2015-10-15 19:23',
+    jobEndTime: '2016-10-15 19:24',
+    jobTimeInterval: '5',
+    jobServers: null
+}
+
+MongoClient.connect('mongodb://localhost:27017/agendaTest', function(err, db) {
+    console.log('mongodb collected--------------');
+    var jobsLogCollection = db.collection('jobsData');
+    agenda.define(job1.jobName, function(_job, done) {
+        console.log(job1.jobName, ' job running: ', new Date());
+        runJob(job1, jobsLogCollection);
+        done();
+    });
+    agenda.every(job1.jobTimeInterval + ' seconds', job1.jobName, job1, job1.jobStartTime, job1.jobEndTime);
+    agenda.start();
+
+    // agenda.define(job2.jobName, function(_job, done) {
+    //     console.log(job2.jobName, ' job running: ', new Date());
+
+    //     runJob(job2, jobsLogCollection);
+    //     done();
+    // });
+    // agenda.every(job2.jobTimeInterval + ' seconds', job2.jobName, job2, job2.jobStartTime, job2.jobEndTime);
+    // agenda.start();
+});
+
+
+
+function runJob(job, jobsLogCollection) {
+
+}
